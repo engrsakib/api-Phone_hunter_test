baca452ec13764c811549e8950ef7af01d54d170
engrsakib <snazmussakib01@gmail.com>
2024-10-05 13:44:26 +0600

    spinner added and its all funconality

--- script/api.js
+++ script/api.js
@@ -1,3 +1,17 @@
 // data-theme
 document.getElementsByTagName('html')[0].setAttribute('data-theme', 'light');
 
+// all connect
+const sipinarShow = document.getElementById('sipinarShow');
+
+const loadPhone = () =>{
+    sipinarShow.classList.add('hidden');
+}
+
+const handelSearch = () =>{
+    sipinarShow.classList.remove('hidden');
+
+    setTimeout(function(){
+        loadPhone();
+    }, 3000);
+}
