2b0722364d1f9d0bc613c53ca71eaa19526140ee
engrsakib <snazmussakib01@gmail.com>
2024-10-05 14:50:36 +0600

    api intregate added

--- script/api.js
+++ script/api.js
@@ -4,8 +4,17 @@ document.getElementsByTagName('html')[0].setAttribute('data-theme', 'light');
 // all connect
 const sipinarShow = document.getElementById('sipinarShow');
 
-const loadPhone = () =>{
+const loadPhone = async () =>{
     sipinarShow.classList.add('hidden');
+
+    const response = await fetch(" https://openapi.programming-hero.com/api/phones?search=iphone");
+    const data = await response.json(); 
+    displayAll(data.data)
+
+}
+
+const displayAll = (phones) =>{
+    console.log(phones);
 }
 
 const handelSearch = () =>{
